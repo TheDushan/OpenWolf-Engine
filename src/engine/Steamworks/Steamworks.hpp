c910dacc3a8597bee6071dae822848094411da87
Dusan Jocic <dusanjocic@msn.com>
2021-05-20 13:32:28 +0200

    [Steamworks] Added initial files for the future Valve Steamworks engine integration

---
+++ src/engine/Steamworks/Steamworks.hpp
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////////////
+// Copyright(C) 2021 Dusan Jocic <dusanjocic@msn.com>
+//
+// This file is part of OpenWolf.
+//
+// OpenWolf is free software; you can redistribute it
+// and / or modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 3 of the License,
+// or (at your option) any later version.
+//
+// OpenWolf is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenWolf; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110 - 1301  USA
+//
+// -------------------------------------------------------------------------------------
+// File name:   Steamworks.cpp
+// Created:
+// Compilers:   Microsoft (R) C/C++ Optimizing Compiler Version 19.26.28806 for x64,
+//              gcc (Ubuntu 9.3.0-10ubuntu2) 9.3.0
+// Description:
+// -------------------------------------------------------------------------------------
+////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __STEAMWORKS_HPP__
+#define __STEAMWORKS_HPP__
+
+//
+// idSteamWorksLocal
+//
+class idSteamWorksLocal : public idSteamWorkSystem {
+public:
+    idSteamWorksLocal(void);
+    ~idSteamWorksLocal(void);
+};
+
+extern idSteamWorksLocal steamWorksLocal;
+
+#endif //!__STEAMWORKS_HPP__
